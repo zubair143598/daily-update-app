98b3232ee5e12c7aa4d715b8bf16047164da2445
M Zubair <md.zubair143598@gmail.com>
2024-12-26 14:41:16 +0500

    [stylistic change] Added filter-invert effect to testimonial icons where needed

--- components/home/testimonials.tsx
+++ components/home/testimonials.tsx
@@ -14,7 +14,7 @@ const testimonials = [
       "Lumunate helped us turn our concept for a customizable maps platform into a reality. Their team designed an intuitive interface, made everything editable in the interface and ensured a flawless user experience.We’ve received rave reviews from our users and the app has seen tremendous growth since its launch.Will be back for future upgrades!",
       h:36,
       w:38,
-      
+      invertProp:"",
   },
   {
     clientImg: '/images/logos/dify.png',
@@ -25,6 +25,7 @@ const testimonials = [
       "Working with the team at Lumunate, in software development, was an excellent experience. As always they went above and beyond, to deliver quality outputs for us, providing post-deployment support. Very proactive communication and a deep understanding, making the whole process smooth and enjoyable!",
       h:23,
       w:38,
+      invertProp:"",
   },
   {
     clientImg: '/images/logos/fast-clean.png',
@@ -35,6 +36,8 @@ const testimonials = [
       "Lumunate helped us turn our concept for a customizable maps platform into a reality. Their team designed an intuitive interface, made everything editable in the interface and ensured a flawless user experience. We’ve received rave reviews from our users and the app has seen tremendous growth since it s launch. Will be back for future upgrades!",
       h:22,
       w:40,
+      invertProp:"",
+
   },
   {
     clientImg: '/images/logos/real-outlet.png',
@@ -45,6 +48,7 @@ const testimonials = [
       "“The team at Lumunate is simply the best. They do the best jobs as always and have brought our idea for Koinfolio to life. Will continue the work with them for sure!",
       h:36,
       w:38,
+      invertProp:"filter invert",
   },
   {
     clientImg: '/images/logos/zeal.png',
@@ -55,6 +59,7 @@ const testimonials = [
       "The team at Lumunate is simply the best. They do the best jobs as always and have brought our idea for Koinfolio to life. Will continue the work with them for sure!",
       w:16,
       h:21,
+      invertProp:"",
   },
   {
     clientImg: '/images/logos/active-campaign.png',
@@ -65,6 +70,7 @@ const testimonials = [
       "“Quite proactive with communication, great attention to detail and very prompt with delivering the final project. 5 stars, would certainly recommend",
       w:102,
       h:21,
+      invertProp:"",
   },
   {
     clientImg: '/images/logos/audiodrome.png',
@@ -75,6 +81,7 @@ const testimonials = [
       "They were great! Took time to understand what I needed, quickly worked to deliver the project for me and then worked with me to make a few revisions to get it exactly how I wanted it. I will definitely work with them again and recommend them to anyone looking for reliable and excellent work",
       w:114,
       h:21,
+      invertProp:"",
   },
   {
     clientImg: '/images/logos/legacy-distrubuting.png',
@@ -85,6 +92,7 @@ const testimonials = [
       "Fantastic job! Was very easy to get this project done and I will definitely use them for future needs, time and time again. Wonderful experience.",
       w:35,
       h:21,
+      invertProp:"",
   },
   {
     clientImg: '/images/logos/koinfolio.png',
@@ -95,6 +103,7 @@ const testimonials = [
       "The team at Lumunate is simply the best. They do the best jobs as always and have brought our idea for Koinfolio to life. Will continue the work with them for sure!",
       w:89.08,
       h:21,
+      invertProp:"",
   },
   {
     clientImg: '/images/logos/ace-my-exam.png',
@@ -105,6 +114,7 @@ const testimonials = [
       "Working with Lumunate was an absolute game-changer for us. We had been struggling with an incomplete app built on Django, and it felt like we’d hit a dead end. The team at Lumunate not only understood our challenges but also took the time to truly grasp our vision. They rebuilt our tutoring platform from the ground up using Next.js, and the difference has been night and day. We couldn’t recommend them more highly!",
       w:100,
       h:21,
+      invertProp:"",
   },
   {
     clientImg: '/images/logos/thomas.png',
@@ -115,6 +125,7 @@ const testimonials = [
       "This was a very big project of mine and needed some helpen from them. They’ve done exactly what was needed to get the end result for my marketing agency. Happy with the expertise and knowledge and looking forward to work more with them in the future. Thanks!",
       w:39,
       h:21,
+      invertProp:"",
   },
   {
     clientImg: '/images/logos/ayse.png',
@@ -125,6 +136,7 @@ const testimonials = [
       "I highly recommend working with Lumunate! The CEO and his team did a great job with helping me build my mobile application and they are clearly experts in their field. They were very easy to work with, polite, communicated efficiently, were transparent about their process, well-organized and had great time management skills. In fact, often times they completed tasks faster than we agreed upon. If you are looking for a high-quality development team, do not hesitate to use Lumunate’s services.",
       w:93,
       h:21,
+      invertProp:"",
   },
 
 ];
@@ -180,6 +192,8 @@ export function Testimonial({
     content: string;
     w:number;
     h:number;
+    invertProp:string;
+
   };
   children: React.ReactNode;
 }) {
@@ -190,7 +204,7 @@ export function Testimonial({
       <div className="flex flex-col gap-4">
         <div className=" flex justify-between">
           <div>
-          <Image width={testimonial.w} src={testimonial.clientImg} height={testimonial.h} alt="Client logo" />
+          <Image className={testimonial.invertProp} width={testimonial.w} src={testimonial.clientImg} height={testimonial.h} alt="Client logo" />
           </div>
           <div>
           <Image className="rounded-sm mr-1" width={40} src={testimonial.flag} height={20} alt="flag" />
