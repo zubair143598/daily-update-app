daf609d17e5e76fc6a69bc8efccb9484acc6e0d0
M Zubair <md.zubair143598@gmail.com>
2025-01-02 11:46:10 +0500

    [stylistic change]changed the navbar max width to 1440px

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -6,7 +6,7 @@ import Logo from "./logo";
 export default function Header() {
   return (
     <header className="z-30 mt-2 w-full md:mt-5">
-      <div className="mx-auto max-w-6xl px-4 sm:px-6">
+      <div className="mx-auto max-w-[1440px] px-4 sm:px-6">
         <div className="relative flex h-14 items-center justify-between gap-3 rounded-2xl bg-forest-900/10 px-3 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,theme(colors.gray.800),theme(colors.gray.700),theme(colors.gray.800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] after:absolute after:inset-0 after:-z-10 after:backdrop-blur-sm">
           {/* Site branding */}
           <div className="flex flex-1 items-center">
