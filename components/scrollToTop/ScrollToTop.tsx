2b3471d6656480467e509cb47d112efb8d423948
M Zubair <md.zubair143598@gmail.com>
2025-01-03 15:28:00 +0500

    [feature added] Added scroll-to-top button

---
+++ components/scrollToTop/ScrollToTop.tsx
@@ -0,0 +1,36 @@
+"use client"
+import styled from 'styled-components';
+import { FaChevronUp } from "react-icons/fa6";
+import React, { useEffect, useState } from 'react';
+
+const StyledArrow = styled(FaChevronUp )`
+  position: fixed;
+  bottom: 40px;
+  right: 50px;
+  z-index: 1000;
+  cursor: pointer;
+  animation: fadeIn 0.3s;
+  transition: opacity 0.4s;
+`;
+
+export default function ScrollToTop() {
+    const [scrollPosition, setScrollPosition] = useState(0);;
+//   const { showScroll, scrollTop } = useScrollToTop({ pageYOffset: 200 });
+
+  useEffect(() => {
+    const handleScroll = () => {
+        setScrollPosition(window.scrollY);
+      };
+      window.addEventListener('scroll', handleScroll);
+      return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+  
+    const showScroll = scrollPosition > 200;
+    return (
+        <>
+      {showScroll && (
+        <StyledArrow size={55} className=' text-forest-800 hover:bg-gray-700 p-2 rounded-full' onClick={() => window.scrollTo(0, 0)} />
+      )}
+    </>
+    );
+  }
