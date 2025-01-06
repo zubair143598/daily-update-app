b88cdde31f25cef6c8d039247d087f5fee2943a5
M Zubair <md.zubair143598@gmail.com>
2025-01-06 15:04:56 +0500

    [stylistic change] Updated testimonial card style to increase the size of the active card

--- components/home/testimonials.tsx
+++ components/home/testimonials.tsx
@@ -6,147 +6,160 @@ import flagsLinks from "./flags/Flags";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import { responsive, useIsDesktopOrAbove } from "@/utils/carouselResponsive";
+import { useState } from "react";
 
 const testimonials = [
   {
-    clientImg: '/images/logos/map-mavens.png',
+    id: 1,
+    clientImg: "/images/logos/map-mavens.png",
     name: "Henry",
     company: "CEO Map Mavens",
     flag: flagsLinks.usa,
     content:
       "Lumunate helped us turn our concept for a customizable maps platform into a reality. Their team designed an intuitive interface, made everything editable in the interface and ensured a flawless user experience.We’ve received rave reviews from our users and the app has seen tremendous growth since its launch.Will be back for future upgrades!",
-      h:36,
-      w:38,
-      invertProp:"",
+    h: 36,
+    w: 38,
+    invertProp: "",
   },
   {
-    clientImg: '/images/logos/dify.png',
+    id: 2,
+    clientImg: "/images/logos/dify.png",
     name: "Luc Elsby",
     company: "Dify.AI",
-    flag:flagsLinks.uk,
+    flag: flagsLinks.uk,
     content:
       "Working with the team at Lumunate, in software development, was an excellent experience. As always they went above and beyond, to deliver quality outputs for us, providing post-deployment support. Very proactive communication and a deep understanding, making the whole process smooth and enjoyable!",
-      w:60,
-      h:38,
-      invertProp:"",
+    w: 60,
+    h: 38,
+    invertProp: "",
   },
   {
-    clientImg: '/images/logos/fast-clean.png',
+    id: 3,
+    clientImg: "/images/logos/fast-clean.png",
     name: "Felicio",
     company: "CEO Fast CLean Service",
-    flag:flagsLinks.netherlands,
+    flag: flagsLinks.netherlands,
     content:
       "Lumunate helped us turn our concept for a customizable maps platform into a reality. Their team designed an intuitive interface, made everything editable in the interface and ensured a flawless user experience. We’ve received rave reviews from our users and the app has seen tremendous growth since it s launch. Will be back for future upgrades!",
-      w:70,
-      h:38,
-      invertProp:"",
-
+    w: 70,
+    h: 38,
+    invertProp: "",
   },
   {
-    clientImg: '/images/logos/real-outlet.png',
+    id: 4,
+    clientImg: "/images/logos/real-outlet.png",
     name: "Ivavisocka",
     company: "CEO The Real Outlet",
-    flag:flagsLinks.latvia,
+    flag: flagsLinks.latvia,
     content:
       "“The team at Lumunate is simply the best. They do the best jobs as always and have brought our idea for Koinfolio to life. Will continue the work with them for sure!",
-      w:52,
-      h:36,
-      invertProp:"filter invert",
+    w: 52,
+    h: 36,
+    invertProp: "filter invert",
   },
   {
-    clientImg: '/images/logos/zeal.png',
+    id: 5,
+    clientImg: "/images/logos/zeal.png",
     name: "Nir",
     company: "",
-    flag:flagsLinks.peru,
+    flag: flagsLinks.peru,
     content:
       "The team at Lumunate is simply the best. They do the best jobs as always and have brought our idea for Koinfolio to life. Will continue the work with them for sure!",
-      w:28,
-      h:36,
-      invertProp:"",
+    w: 28,
+    h: 36,
+    invertProp: "",
   },
   {
-    clientImg: '/images/logos/active-campaign.png',
+    id: 6,
+    clientImg: "/images/logos/active-campaign.png",
     name: "Ofirsilbiger",
     company: "",
-    flag:flagsLinks.sweden,
+    flag: flagsLinks.sweden,
     content:
       "“Quite proactive with communication, great attention to detail and very prompt with delivering the final project. 5 stars, would certainly recommend",
-      w:120,
-      h:36,
-      invertProp:"",
+    w: 120,
+    h: 36,
+    invertProp: "",
   },
   {
-    clientImg: '/images/logos/audiodrome.png',
+    id: 7,
+    clientImg: "/images/logos/audiodrome.png",
     name: "Dave Newell",
     company: "",
-    flag:flagsLinks.usa,
+    flag: flagsLinks.usa,
     content:
       "They were great! Took time to understand what I needed, quickly worked to deliver the project for me and then worked with me to make a few revisions to get it exactly how I wanted it. I will definitely work with them again and recommend them to anyone looking for reliable and excellent work",
-      w:122,
-      h:36,
-      invertProp:"",
+    w: 122,
+    h: 36,
+    invertProp: "",
   },
   {
-    clientImg: '/images/logos/legacy-distrubuting.png',
+    id: 8,
+    clientImg: "/images/logos/legacy-distrubuting.png",
     name: "Katelyn",
     company: "Legacy Distributing",
-    flag:flagsLinks.usa,
+    flag: flagsLinks.usa,
     content:
       "Fantastic job! Was very easy to get this project done and I will definitely use them for future needs, time and time again. Wonderful experience.",
-      w:50,
-      h:36,
-      invertProp:"",
+    w: 50,
+    h: 36,
+    invertProp: "",
   },
   {
-    clientImg: '/images/logos/koinfolio.png',
+    id: 9,
+    clientImg: "/images/logos/koinfolio.png",
     name: "Ebrahim",
     company: "CEO Koinfolio",
-    flag:flagsLinks.germany,
+    flag: flagsLinks.germany,
     content:
       "The team at Lumunate is simply the best. They do the best jobs as always and have brought our idea for Koinfolio to life. Will continue the work with them for sure!",
-      w:96,
-      h:21,
-      invertProp:"",
+    w: 96,
+    h: 21,
+    invertProp: "",
   },
   {
-    clientImg: '/images/logos/ace-my-exam.png',
+    id: 10,
+    clientImg: "/images/logos/ace-my-exam.png",
     name: "Asma",
     company: "CEO Acemyexam",
-    flag:flagsLinks.uk,
+    flag: flagsLinks.uk,
     content:
       "Working with Lumunate was an absolute game-changer for us. We had been struggling with an incomplete app built on Django, and it felt like we’d hit a dead end. The team at Lumunate not only understood our challenges but also took the time to truly grasp our vision. They rebuilt our tutoring platform from the ground up using Next.js, and the difference has been night and day. We couldn’t recommend them more highly!",
-      w:110,
-      h:36,
-      invertProp:"",
+    w: 110,
+    h: 36,
+    invertProp: "",
   },
   {
-    clientImg: '/images/logos/thomas.png',
+    id: 11,
+    clientImg: "/images/logos/thomas.png",
     name: "Thomas",
     company: "The Marketing Captain",
-    flag:flagsLinks.netherlands,
+    flag: flagsLinks.netherlands,
     content:
       "This was a very big project of mine and needed some helpen from them. They’ve done exactly what was needed to get the end result for my marketing agency. Happy with the expertise and knowledge and looking forward to work more with them in the future. Thanks!",
-      w:60,
-      h:36,
-      invertProp:"",
+    w: 60,
+    h: 36,
+    invertProp: "",
   },
   {
-    clientImg: '/images/logos/ayse.png',
+    id: 12,
+    clientImg: "/images/logos/ayse.png",
     name: "Cathy Charles",
     company: "CEO Ayse",
-    flag:flagsLinks.usa,
+    flag: flagsLinks.usa,
     content:
       "I highly recommend working with Lumunate! The CEO and his team did a great job with helping me build my mobile application and they are clearly experts in their field. They were very easy to work with, polite, communicated efficiently, were transparent about their process, well-organized and had great time management skills. In fact, often times they completed tasks faster than we agreed upon. If you are looking for a high-quality development team, do not hesitate to use Lumunate’s services.",
-      w:103,
-      h:32,
-      invertProp:"",
+    w: 103,
+    h: 32,
+    invertProp: "",
   },
-
 ];
 
 export default function Testimonials() {
   const isDesktopOrAbove = useIsDesktopOrAbove();
   const masonryContainer = useMasonry();
+  //state to track active card
+  const [activeIndex, setActiveIndex] = useState(1);
 
   return (
     <div id="testimonials" className="mx-auto max-w-[1440px] px-4 sm:px-6">
@@ -167,12 +180,12 @@ export default function Testimonials() {
         <div>
           {/* Cards */}
           <div
-            className="mx-auto max-w-sm items-start gap-x-6 space-x-9 sm:max-w-none "
+            className="mx-auto max-w-sm items-start gap-x-6 space-x-9 sm:max-w-none"
             ref={masonryContainer}
           >
             <Carousel
               responsive={responsive}
-              autoPlay={false}
+              autoPlay={true}
               autoPlaySpeed={3000}
               infinite={true}
               arrows={true}
@@ -180,16 +193,22 @@ export default function Testimonials() {
               centerMode={isDesktopOrAbove} // Enable center mode
               focusOnSelect={true} // Focus the center item when clicked
               customTransition="transform 300ms ease-in-out"
+              afterChange={(nextSlide) => setActiveIndex(nextSlide)} 
             >
               {testimonials.map((testimonial, index) => (
-                <div
-                  data-aos="fade-up"
-                  key={index}
-                  className="group flex h-full sm:w-[640px] w-[358px]"
-                >
-                  <Testimonial testimonial={testimonial}>
-                    {testimonial.content}
-                  </Testimonial>
+                <div key={testimonial.id}
+                className={`p-5 transition-all duration-100 ease-in-out overflow-hidden ${
+                  activeIndex === testimonial.id ? "md:h-[350px] h-full" : "h-full md:h-auto "
+                }`}>
+                  <div
+                    data-aos="fade-up"
+                    key={index}
+                    className={`group flex h-full sm:w-[640px] w-[358px]`}
+                  >
+                    <Testimonial testimonial={testimonial}>
+                      {testimonial.content}
+                    </Testimonial>
+                  </div>
                 </div>
               ))}
             </Carousel>
@@ -211,16 +230,15 @@ export function Testimonial({
   testimonial,
   children,
 }: {
-    testimonial: {
+  testimonial: {
     clientImg: string;
     name: string;
     company: string;
-    flag:string;
+    flag: string;
     content: string;
-    w:number;
-    h:number;
-    invertProp:string;
-
+    w: number;
+    h: number;
+    invertProp: string;
   };
   children: React.ReactNode;
 }) {
@@ -231,12 +249,23 @@ export function Testimonial({
       <div className="flex flex-col h-full justify-between gap-4">
         <div className=" flex justify-between">
           <div>
-          <Image className={testimonial.invertProp} width={testimonial.w} src={testimonial.clientImg} height={testimonial.h} alt="Client logo" />
+            <Image
+              className={testimonial.invertProp}
+              width={testimonial.w}
+              src={testimonial.clientImg}
+              height={testimonial.h}
+              alt="Client logo"
+            />
           </div>
           <div>
-          <Image className="rounded-sm mr-1" width={40} src={testimonial.flag} height={20} alt="flag" />
+            <Image
+              className="rounded-sm mr-1"
+              width={40}
+              src={testimonial.flag}
+              height={20}
+              alt="flag"
+            />
           </div>
-          
         </div>
         <p className="text-[#838AA7] before:content-['“'] after:content-['”']">
           {children}
@@ -244,12 +273,14 @@ export function Testimonial({
         <div className="flex items-center gap-3">
           <div className="text-sm font-medium text-gray-200">
             <span>{testimonial.name}</span>
-            { testimonial.company ? <span className="text-gray-700"> - </span>:""}
-    <span
-      className="text-forest-200/65 transition-colors hover:text-forest-500"
-    >
-      {testimonial.company}
-    </span>
+            {testimonial.company ? (
+              <span className="text-gray-700"> - </span>
+            ) : (
+              ""
+            )}
+            <span className="text-forest-200/65 transition-colors hover:text-forest-500">
+              {testimonial.company}
+            </span>
           </div>
         </div>
       </div>
